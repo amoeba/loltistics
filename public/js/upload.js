7963987012e97a3a669fac1da9837c47ab1f4b1e
Bryce Mecum <petridish@gmail.com>
2010-07-21 15:31:55 -0800

    Make uploading display status

--- public/js/upload.js
+++ public/js/upload.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
   console.log("Uploader init");
   
   $('#upload-form').ajaxForm({
+    beforeSubmit: function(arr, $form, options) {
+      $("#output").append("<li><img src=\"/images/spinner.gif\" alt=\"(Working) \"/>Processing " + $form.children(":file").first().val() + "</li>");
+    },
     success: function(responseText, statusText, xhr, $form) {
-      $("#output")
+      $("#output li").last().html(responseText);
+      $form.resetForm();
     }
   });
 });

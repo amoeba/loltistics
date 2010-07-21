e3187c18e00930983815ffd12a69422ed1279045
Bryce Mecum <petridish@gmail.com>
2010-07-20 16:39:05 -0800

    Removing explicit sass line from gems manifest as it's not needed

--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
+require 'mongo'
+require 'haml'
+require 'sass'
 
+set :haml, {:format => :html5 }
+
+get '/stylesheets/loltistics.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass :loltistics
+end
+  
 get '/' do
-  "Hello World!"
+  haml :index
+end
+
+get '/match/:id' do |match|
+  @match = match
+  
+  haml :match
+end
+
+get '/player/:name' do |player|
+  @player = player
+  
+  haml :player
 end

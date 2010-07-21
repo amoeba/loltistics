6fde5207d7a1219f256d573624381aeea239b10c
Bryce Mecum <petridish@gmail.com>
2010-07-21 15:43:57 -0800

    Allow multiple responses to come back at the same time

--- public/js/upload.js
+++ public/js/upload.js
@@ -3,11 +3,19 @@ $(document).ready(function() {
   
   $('#upload-form').ajaxForm({
     beforeSubmit: function(arr, $form, options) {
-      $("#output").append("<li><img src=\"/images/spinner.gif\" alt=\"(Working) \"/>Uploading and Processing " + $form.children(":file").first().val() + "</li>");
+      var filename = $form.children(":file").first().val();
+      $("#output").append("<li data-filename=\"" + filename + "\"><img src=\"/images/spinner.gif\" alt=\"(Working) \"/>Uploading and Processing " + filename + "</li>");
     },
     success: function(responseText, statusText, xhr, $form) {
-      $("#output li").last().html(responseText);
-      $form.resetForm();
+      $("#output li").each(function() {
+        var filename = $form.children(":file").first().val();
+        var current_filename = $(this).attr('data-filename')
+        
+        if(current_filename && filename == current_filename) {
+          $(this).html(responseText);
+        }
+      });
+      $form.resetFields();
     }
   });
 });

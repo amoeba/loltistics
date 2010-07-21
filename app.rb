e0b9c8fafe6d4ff89c2bbb720c709fa52a04d033
Bryce Mecum <petridish@gmail.com>
2010-07-21 10:21:22 -0800

    Add MongoDB config

--- app.rb
+++ app.rb
@@ -1,7 +1,29 @@
 require 'sinatra'
+require 'uri'
 require 'mongo'
 require 'haml'
 require 'sass'
+
+require './lib/lol/lol'
+
+# MongoHQ
+if ENV['MONGOHQ_URL']
+  # We're on Heroku and should use MongoHQ
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  connection = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  db = connection.db(uri.path.gsub(/^\//, ''))
+  
+  #db = Mongo::Connection.new('flame.mongohq.com', 27078).db('loltistics')
+  #auth = db.authenticate('amoeba', 'Ne2uMh')
+else
+  connection = Mongo::Connection.new('127.0.0.1', 27017)
+  db = connection.db('loltistics')
+end
+
+if db
+  matches = db.collection('matches')
+  players = db.collection('players')
+end
 
 set :haml, {:format => :html5 }
 
@@ -14,14 +36,44 @@ get '/' do
   haml :index
 end
 
-get '/match/:id' do |match|
-  @match = match
+get '/matches' do
+  @matches = matches.find()
+  
+  haml :matches
+end
+
+get '/matches/:id' do |id|
+  @match = matches.find_one('id' => id)
   
   haml :match
 end
 
-get '/player/:name' do |player|
-  @player = player
+get '/players/:name' do |name|
+  @player = players.find_one('summoner_name' => name)
   
   haml :player
+end
+
+get '/upload' do
+  haml :upload
+end
+
+post '/upload' do
+  @result = LOL.parse_file(params[:file][:tempfile].read)
+  
+  @result[:matches].each do |k, v|
+    matches.insert(v) unless matches.find_one({'id' => k})
+  end
+
+  @result[:players].each do |k, v|
+    existing_player = players.find_one('summoner_name' => v[:summoner_name])
+  
+    if existing_player
+      players.save(existing_player) unless existing_player['last_game_timestamp'] < v[:last_game_timestamp]
+    else
+      players.insert(v)
+    end
+  end
+
+  haml :result
 end
